5f4046fa7aa428d5a0400ce0cb726ebdd4a2b0da
pipych <107928032+pipych@users.noreply.github.com>
2026-07-23 18:30:08 +0300

    Create config.js

---
+++ app/api/config.js
@@ -0,0 +1,13 @@
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  return res.status(200).json({
+    supabaseUrl: process.env.SUPABASE_URL,
+    supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+  });
+};
