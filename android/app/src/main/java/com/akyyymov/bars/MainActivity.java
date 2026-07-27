fe744de3f011a7d7995e52f9abe65ea790dab5c5
akyyymov <akyyymov@gmail.com>
2026-07-27 18:16:44 +0300

    feat: in-app WebView OAuth (no CCT, no deep links) - Chrome UA + allowNavigation

--- android/app/src/main/java/com/akyyymov/bars/MainActivity.java
+++ android/app/src/main/java/com/akyyymov/bars/MainActivity.java
@@ -1,5 +1,17 @@
 package com.akyyymov.bars;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStart() {
+        super.onStart();
+        WebView webView = getBridge().getWebView();
+        // Set Chrome user agent to bypass Google's WebView OAuth block
+        webView.getSettings().setUserAgentString(
+            "Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36"
+        );
+    }
+}
